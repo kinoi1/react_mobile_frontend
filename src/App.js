35e4196f6efa0b069130c7db18346852f2074012
kinoi1 <agilsupriyanto14@gmail.com>
2024-11-06 22:34:01 +0700

    migrasi tailwind ke bootstrap 4

--- src/App.js
+++ src/App.js
@@ -5,32 +5,37 @@ function App() {
   return (
     <div>
       <section className="bg-main pt-11 pb-14">
-        <div className="container">
+        <div className="container pl-6">
           <div className="row">
-            <div className="flex flex-col px-4">
-              <div className="text-white">
-                <span>Halo, Jeni</span>
-              </div>
-              <div>
-                <span>apa kabar</span>
-              </div>
-              <div className="bg-white w-11/12 p-4 rounded-xl absolute top-28 shadow-lg">
-                <div className="flex flex-row gap-4">
-                  <div className="flex flex-col">
-                    <div className="text-slate-400">Saldo Tersedia</div>
-                    <div className="text-sky-600">Rp. 150.000</div>
-                  </div>
-                  <div className="flex flex-col">
-                    <div className="bg-slate-200 rounded-lg">
-                      <p className="text-sky-600 flex justify-center">1351</p>
+            <div className='col-md-12'>
+              <div className="d-flex flex-column">
+                <div className="text-white">
+                  <span>Halo, Jeni</span>
+                </div>
+                <div>
+                  <span className="text-slate-400">apa kabar</span>
+                </div>
+                <div className="w-93 bg-white card p-4 rounded-xl absolute top-16 shadow-lg">
+                  <div className="d-flex flex-row gap-4">
+                    <div className="d-flex flex-column pt-3">
+                      <div className="small">Saldo Tersedia</div>
+                      <div className="lead">Rp. 150.000</div>
                     </div>
-                    <div className="text-slate-400">Pekerjaan</div>
-                  </div>
-                  <div className="flex flex-col">
-                    <div className="bg-slate-200 rounded-lg">
-                      <p className="text-sky-600 flex justify-center">Rp 500k</p>
+                    <div className='d-flex align-self-end'>
+                        <span className='fa fa-coins'></span>
                     </div>
-                    <div className="text-slate-400">Penarikan</div>
+                    <div className="d-flex flex-column">
+                      <div className="col-md-12 bg-slate-200 rounded-xl d-flex">
+                        <p className="text-sky-600 flex align-self-center m-0 pb-2 pt-2">1351</p>
+                      </div>
+                      <div className="text-slate-400">Pekerjaan</div>
+                    </div>
+                    <div className="d-flex flex-column">
+                      <div className="col-md-12 bg-slate-200 rounded-xl d-flex px-2-5">
+                        <p className="text-sky-600 flex justify-center m-0 pb-2 pt-2">Rp 500k</p>
+                      </div>
+                      <div className="text-slate-400">Penarikan</div>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -39,9 +44,9 @@ function App() {
         </div>
       </section>
 
-      <section className="pt-16">
-        <div className="container ml-4">
-          <div className="w-11/12 bg-sky-600 rounded-lg">
+      <section className="pt-24">
+        <div className="container ml-4 p-0">
+          <div className="w-90 bg-sky-600 rounded-xl shadow-lg card">
             <div className="pl-2">
               <div className="flex flex-col">
                 <p>Kini Notif Job Langsung di Whatsapp</p>
@@ -57,11 +62,43 @@ function App() {
       </section>
 
       <section className="pt-8">
-        <div className="container mx-4">
-          <div className="w-11/12 flex flex-row gap-1">
-            <p className="w-6/12 mr-2">Pekerjaan aktif</p>
-            <div className="bg-rose-500 text-white rounded-lg px-2 ml-8">
-              Segera Kerjakan!
+        <div className="container pl-6">
+          <div className='row'>
+            <div className="col-md-12 d-flex flex-row gap-1">
+              <p className="col-md-8 m-0 mr-2 p-0">Pekerjaan aktif</p>
+              <div className="col-md-4 bg-rose-500 text-white rounded-lg px-2 ml-8 text-right">
+                Segera Kerjakan!
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </section>
+
+      <section className="pt-8 work-active">
+        <div className="bg-white w-11/12 p-4 rounded-xl shadow-lg">
+          <div className='w-full'>
+            <div class="flex flex-row">
+              <div className='bg-slate-200 rounded-lg'>
+                
+              </div>
+              <div className="flex flex-row">
+                <div className='flex flex-col'>
+                  <div className='text-slate-400'>
+                    <p>Follow Tiktok</p>
+                  </div>
+                  <div className='text-sky-600'>
+                    <p><i></i> Rp. 500</p>
+                  </div>
+                </div>
+               
+                <div className='flex flex-col'>
+                  <span>Progress</span>
+                  <div className="bg-blue-600 h-4 rounded-full progress-50">
+
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
